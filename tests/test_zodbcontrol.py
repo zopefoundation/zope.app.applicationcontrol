eea6bb3c3453e6ceba71fa90c738461cc70771eb
Fred Drake <fdrake@acm.org>
2003-08-26 16:22:31 +0000

    - make these tests clean up after themselves - normalize whitespace

--- tests/test_zodbcontrol.py
+++ tests/test_zodbcontrol.py
@@ -12,15 +12,15 @@
 ##############################################################################
 """Runtime Info Tests
 
-$Id: test_zodbcontrol.py,v 1.1 2003/07/31 21:37:23 srichter Exp $
+$Id: test_zodbcontrol.py,v 1.2 2003/08/26 16:22:31 fdrake Exp $
 """
 import unittest
 import os
 
 from zodb.storage.file import FileStorage
 from zodb.db import DB
-from zope.app.applicationcontrol import tests 
-from zope.app.applicationcontrol.zodbcontrol import ZODBControl 
+from zope.app.applicationcontrol import tests
+from zope.app.applicationcontrol.zodbcontrol import ZODBControl
 from zope.app.applicationcontrol.applicationcontrol import applicationController
 
 
@@ -28,17 +28,20 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         db_file = os.path.join(os.path.dirname(tests.__file__), 'zodb.fs')
-        self.db = DB(FileStorage(db_file), '')
+        self.storage = FileStorage(db_file)
+        self.db = DB(self.storage, '')
         self.control = ZODBControl(applicationController)
+
+    def tearDown(self):
+        self.db.close()
+        self.storage.cleanup()
 
     def test_getDatabaseSize(self):
         self.assertEqual(self.control.getDatabaseSize(self.db), 1183)
 
 
 def test_suite():
-    return unittest.TestSuite((
-        unittest.makeSuite(Test),
-        ))
+    return unittest.makeSuite(Test)
 
 if __name__ == '__main__':
     unittest.main()
