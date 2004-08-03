ffdf3794322e7ed2831f17a51073ae21e28f04b0
Fred Drake <fdrake@acm.org>
2004-08-03 21:19:09 +0000

    look for the version.txt magic file in the zope.app package instead of zope

--- zopeversion.py
+++ zopeversion.py
@@ -18,7 +18,7 @@ $Id$
 import os
 import re
 
-import zope
+import zope.app
 from zope.app.applicationcontrol.interfaces import IZopeVersion
 from zope.interface import implements
 
@@ -30,7 +30,7 @@ class ZopeVersion(object):
 
     def __init__(self, path=None):
         if path is None:
-            path = os.path.dirname(os.path.abspath(zope.__file__))
+            path = os.path.dirname(os.path.abspath(zope.app.__file__))
         self.path = path
         self.result = None
 
