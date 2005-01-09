18e4da45375eee17ebaf67404d2591b0fef029d7
Stephan Richter <stephan.richter@gmail.com>
2005-01-09 18:18:04 +0000

    Deactivted all BBB to ensure that we use only the latest code in zope.app
    Made almost all tests pass again, except the few that heavily depended on
    the old way of doing things: apidoc, module, presentation

---
+++ browser/tests/test_runtimeinfoview.py
@@ -0,0 +1,79 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002, 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Runtime View tests
+
+$Id$
+"""
+import unittest
+from types import DictType
+from zope.app.testing import ztapi
+
+from zope.app.applicationcontrol.applicationcontrol import applicationController
+from zope.app.applicationcontrol.runtimeinfo import RuntimeInfo
+from zope.app.applicationcontrol.browser.runtimeinfo import RuntimeInfoView
+from zope.app.applicationcontrol.interfaces import \
+     IApplicationControl, IRuntimeInfo
+from zope.app.component.testing import PlacefulSetup
+
+class Test(PlacefulSetup, unittest.TestCase):
+
+    def _TestView__newView(self, container):
+        view = RuntimeInfoView()
+        view.context = container
+        view.request = None
+        return view
+
+    def test_RuntimeInfoView(self):
+        ztapi.provideAdapter(IApplicationControl, IRuntimeInfo, RuntimeInfo)
+        test_runtimeinfoview = self._TestView__newView(applicationController)
+
+        test_format = test_runtimeinfoview.runtimeInfo()
+        self.failUnless(isinstance(test_format, DictType))
+
+        assert_keys = ['ZopeVersion', 'PythonVersion', 'PythonPath',
+              'SystemPlatform', 'PreferredEncoding', 'FileSystemEncoding',
+              'CommandLine', 'ProcessId', 'Uptime' ]
+        test_keys = test_format.keys()
+
+        assert_keys.sort()
+        test_keys.sort()
+        self.failUnless(assert_keys == test_keys)
+
+        self.failUnless(test_format["ZopeVersion"] != "n/a")
+
+    def test_RuntimeInfoFailureView(self):
+        test_runtimeinfoview = self._TestView__newView(applicationController)
+
+        test_format = test_runtimeinfoview.runtimeInfo()
+        self.failUnless(isinstance(test_format, DictType))
+
+        assert_keys = ['ZopeVersion', 'PythonVersion', 'PythonPath',
+              'SystemPlatform', 'PreferredEncoding', 'FileSystemEncoding',
+              'CommandLine', 'ProcessId', 'Uptime', 'Hint']
+        test_keys = test_format.keys()
+
+        assert_keys.sort()
+        test_keys.sort()
+        self.failUnless(assert_keys == test_keys)
+
+        self.failUnless(test_format["ZopeVersion"] == "n/a")
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(Test),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ browser/tests/test_servercontrolview.py
@@ -0,0 +1,73 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002, 2003 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Server Control View Tests
+
+$Id$
+"""
+import unittest
+
+from zope.interface import implements
+from zope.app import zapi
+from zope.app.applicationcontrol.applicationcontrol import applicationController
+from zope.app.applicationcontrol.browser.servercontrol import ServerControlView
+from zope.app.applicationcontrol.interfaces import IServerControl
+from zope.app.servicenames import Utilities
+from zope.app.component.testing import PlacefulSetup
+
+class ServerControlStub(object):
+    implements(IServerControl)
+    
+    did_restart = None
+    did_shutdown = None
+
+    def restart(self, time):
+        self.did_restart = time
+
+    def shutdown(self, time):
+        self.did_shutdown = time
+
+class Test(PlacefulSetup, unittest.TestCase):
+
+    def _TestView__newView(self, container, request):
+        view = ServerControlView()
+        view.context = container
+        view.request = request
+        return view
+
+    def test_ServerControlView(self):
+        control = ServerControlStub()
+        zapi.getGlobalSiteManager().provideUtility(IServerControl, control)
+
+        test_serverctrl = self._TestView__newView(
+            applicationController,
+            {'shutdown': 1},
+            )
+        test_serverctrl.action(100)
+        self.assertEqual(control.did_shutdown, 100)
+
+        test_serverctrl = self._TestView__newView(
+            applicationController,
+            {'restart': 1},
+            )
+        test_serverctrl.action(100)
+        self.assertEqual(control.did_restart, 100)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        unittest.makeSuite(Test),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
