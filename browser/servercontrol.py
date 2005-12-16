855fa8d9b46048d5591d6eb071993133a547e27b
Benji York <benji@benjiyork.com>
2005-12-16 21:32:26 +0000

    XXX and whitespace fixes

---
+++ browser/servercontrol.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Server Control View
+
+$Id$
+"""
+__docformat__ = 'restructuredtext'
+
+from zope.app import zapi
+from zope.app.applicationcontrol.interfaces import IServerControl
+
+from zope.app.i18n import ZopeMessageFactory as _
+
+class ServerControlView(object):
+
+    def serverControl(self):
+        return zapi.getUtility(IServerControl)
+
+    def action(self):
+        """Do the shutdown/restart!"""
+        # TODO I18N the responses
+        control = self.serverControl()
+
+        if 'time' in self.request:
+            time = self.request.get('time', 0)
+
+        if 'restart' in self.request:
+            control.restart(time)
+            return u"The server will be restarted in %s seconds." % time
+        elif 'shutdown' in self.request:
+            control.shutdown(time)
+            return u"The server will be shutdown in %s seconds." % time
