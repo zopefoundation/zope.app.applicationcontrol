23e0e46a488c4c2e39e54987449747f61246a82a
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-01-17 04:55:46 +0000

    Initial commit of zope.app.applicationcontrol for eggification

---
+++ README.txt
@@ -0,0 +1,37 @@
+zope.app.applicationcontrol Package Readme
+==========================================
+
+Overview
+--------
+
+The application control instance is usually generated upon startup.
+This package provides runtime information adapter for application
+control and Zope version.  Also provide a utility with methods for
+shutting down and restarting the server.
+
+Changes
+-------
+
+See CHANGES.txt.
+
+Installation
+------------
+
+See INSTALL.txt.
+
+
+Developer Resources
+-------------------
+
+- Subversion browser:
+
+  http://svn.zope.org/zope.app.applicationcontrol/
+
+- Read-only Subversion checkout:
+
+  $ svn co svn://svn.zope.org/repos/main/zope.app.applicationcontrol/trunk
+
+- Writable Subversion checkout:
+
+  $ svn co svn+ssh://userid@svn.zope.org/repos/main/zope.app.applicationcontrol/trunk
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.applicationcontrol package
+
+$Id$
+"""
+
+import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.app.applicationcontrol',
+      version='3.4-dev',
+      url='http://svn.zope.org/zope.app.applicationcontrol',
+      license='ZPL 2.1',
+      description='Zope applicationcontrol',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description="The application control instance is usually"
+                       "generated upon startup.  This package provides"
+                       "runtime information adapter for application"
+                       "control and Zope version.  Also provide a utility"
+                       "with methods for shutting down and"
+                       "restarting the server.",
+
+      packages=['zope', 'zope.app',
+                'zope.app.applicationcontrol',
+                'zope.app.applicationcontrol.browser',
+                'zope.app.applicationcontrol.browser.ftests',
+                'zope.app.applicationcontrol.browser.tests',
+                'zope.app.applicationcontrol.tests'],
+      package_dir = {'': 'src'},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      install_requires=['zope.interface',
+                        'zope.i18n',
+                        'zope.app.i18n',
+                        'zope.size'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
