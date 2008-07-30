83ba0c33e7734a8fd2926c5a94a7d45f67f83419
Marius Gedminas <marius@gedmin.as>
2008-07-30 18:31:41 +0000

    Prepare to release 3.4.3 (a bit of a brown-paper-bag-bug).

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.applicationcontrol',
-    version = '3.4.3dev',
+    version = '3.4.3',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description='Zope applicationcontrol',
