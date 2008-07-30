8658ca4e697fdb27d475e9e0485d051428963853
Marius Gedminas <marius@gedmin.as>
2008-07-30 16:31:35 +0000

    Fixed deprecation warning in ftesting.zcml: import ZopeSecurityPolicy from the new location.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.applicationcontrol',
-    version = '3.4.1',
+    version = '3.4.2dev',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description='Zope applicationcontrol',
