bc68e1530b4ab32235559a1ef6e73a4491c0e1a2
Shane Hathaway <shane@hathawaymix.org>
2009-05-23 22:10:13 +0000

    post release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.applicationcontrol',
-    version = '3.5.0',
+    version = '3.5.1dev',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description='Zope applicationcontrol',
