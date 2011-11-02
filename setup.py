f1a6aada338d4e657a5faf68d5970c5f259aca46
Tres Seaver <tseaver@palladion.com>
2011-11-02 15:08:50 +0000

    Prep 3.5.10 release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '3.5.10dev'
+version = '3.5.10'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
